52272f7fed7d4eea362f9681143a0015f3af4c76
Ali <aalnasih4846@gmail.com>
2026-01-31 12:41:49 -0500

    Add files

---
+++ Sprites_01/Tilesheet/new_script.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@export var tile_size := 64
+@export var move_time := 0.12
+
+var moving := false
+
+func _process(_delta):
+	if moving:
+		return
+
+	var dir := Vector2.ZERO
+
+	if Input.is_action_just_pressed("ui_right"):
+		dir = Vector2.RIGHT
+	elif Input.is_action_just_pressed("ui_left"):
+		dir = Vector2.LEFT
+	elif Input.is_action_just_pressed("ui_down"):
+		dir = Vector2.DOWN
+	elif Input.is_action_just_pressed("ui_up"):
+		dir = Vector2.UP
+
+	if dir != Vector2.ZERO:
+		try_move(dir)
+
+func try_move(dir: Vector2):
+	var target_pos = position + dir * tile_size
+
+	# collision check
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = target_pos
+	query.collide_with_areas = false
+	query.collide_with_bodies = true
+
+	if space.intersect_point(query).is_empty():
+		move_to(target_pos)
+
+func move_to(target_pos: Vector2):
+	moving = true
+	var tween = create_tween()
+	tween.tween_property(self, "position", target_pos, move_time)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_OUT)
+	tween.finished.connect(func(): moving = false)
