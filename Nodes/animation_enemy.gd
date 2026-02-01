2115d401f86418a0ec3424610babb80159173fc5
EwickVon-1 <ericvo.1227207@gmail.com>
2026-02-01 16:46:26 -0500

    Final

--- Nodes/animation_enemy.gd
+++ Nodes/animation_enemy.gd
@@ -1,29 +1,4 @@
-extends AnimationPlayer
+extends Node2D
 
-@onready var enemy = get_node("../../Enemy")
-@onready var sprite = get_node("../Sprite2D")
-var rundir = "right"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	animation_finished.connect(_on_animation_finished)
-	speed_scale = 4
-	play("idle") # Replace with function body.
-
-func _on_animation_finished(anim_name):
-	speed_scale = 4
-	play("idle")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	speed_scale = 8
-	play("run" + rundir)
-	if rundir == "right":
-		rundir = "left"
-	else:
-		rundir = "right"
-	if enemy.dirfacing == "right":
-		sprite.flip_h = false
-	else:
-		sprite.flip_h = true
-		sprite.offset.x = 8
+func _ready() -> void:
+	pass
